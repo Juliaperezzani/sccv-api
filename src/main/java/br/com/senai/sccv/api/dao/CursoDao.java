a66b868a17a3647d0047506ac0b7bd89e3e037b3
Juliaperezzani <42803849+Juliaperezzani@users.noreply.github.com>
2018-10-29 12:19:10 -0200

    a

---
+++ src/main/java/br/com/senai/sccv/api/dao/CursoDao.java
@@ -0,0 +1,16 @@
+package br.com.senai.sccv.api.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.List;
+
+import br.com.senai.sccv.api.vo.ClasseGenerica;
+
+public class CursoDao {
+	
+	Connection con;
+	PreparedStatement ps;
+	
+	public List<ClasseGenerica> listarCursoPorFiltro 
+
+}
---
+++ src/main/java/br/com/senai/sccv/api/services/CursoService.java
@@ -0,0 +1,18 @@
+package br.com.senai.sccv.api.services;
+
+import java.util.List;
+
+import br.com.senai.sccv.api.dao.CursoDao;
+import br.com.senai.sccv.api.vo.ClasseGenerica;
+
+public class CursoService {
+	
+	CursoDao rDao;
+	
+	public CursoService() {
+		rDao = new CursoDao();
+	}
+	
+	public List<ClasseGenerica> listarCursosPorArea 
+
+}
