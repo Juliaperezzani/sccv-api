b863a30edfddbc65563d708511b19c51d2b56007
Juliaperezzani <42803849+Juliaperezzani@users.noreply.github.com>
2018-11-07 09:27:44 -0200

    a

--- src/main/java/br/com/senai/sccv/api/services/SemestreService.java
+++ src/main/java/br/com/senai/sccv/api/services/SemestreService.java
@@ -1,10 +1,17 @@
 package br.com.senai.sccv.api.services;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import br.com.senai.sccv.api.dao.SemestreDao;
+import br.com.senai.sccv.api.vo.ClasseGenerica;
+import br.com.senai.sccv.api.vo.Filtro;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
@@ -16,6 +23,20 @@ public class SemestreService {
 		sDao = new SemestreDao();
 	}
 	
-
+	@POST
+	@Path("/semestre")
+	public List<ClasseGenerica> listarSemestrePorCurso (Filtro filtro){
+		try {
+			
+			return sDao.listarSemestrePorFiltro(filtro);
+			
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+ 
 
 }
