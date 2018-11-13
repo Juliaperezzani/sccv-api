9324d64cdd896b9b404d66962bc818c1186386a9
Nailton <>
2018-11-13 10:28:13 -0200

    banco

--- src/main/java/br/com/senai/sccv/api/vo/Filtro.java
+++ src/main/java/br/com/senai/sccv/api/vo/Filtro.java
@@ -6,27 +6,18 @@ public class Filtro {
 
 	private String nomeFiltro;
 	private Integer id;
-	private Integer idCurso; // vai
+	private Integer idCurso; 
 	private Integer semestre; // vai
-	private List<Integer> idCidade; // vai
+	private List<Integer> idCidade; 
 	private Integer experiencia; // 0=indiferente 1=SIM 2= NÃO
 	private Integer sexo; // vai 0=indiferente 1=MACHO 2= MULHER
-	private boolean idioma;// vai
-	private Integer qualIdioma;// vai 0=indiferente 1=INGLES 2= ESPANHOL
 	private Integer deficiencia;// vai 0=indiferente 1=SIM 2= NÃO
-	private Integer idade_inicio;
-	private Integer idade_fim;
+	private Integer idade_inicio;//
+	private Integer idade_fim; //
 	private Integer area;
 
 	// nome, idade inicoon idade fim area
 
-	public boolean isIdioma() {
-		return idioma;
-	}
-
-	public void setIdioma(boolean idioma) {
-		this.idioma = idioma;
-	}
 
 	public Integer getId() {
 		return id;
@@ -108,14 +99,6 @@ public class Filtro {
 
 	public void setSexo(Integer sexo) {
 		this.sexo = sexo;
-	}
-
-	public Integer getQualIdioma() {
-		return qualIdioma;
-	}
-
-	public void setQualIdioma(Integer qualIdioma) {
-		this.qualIdioma = qualIdioma;
 	}
 
 	public Integer getDeficiencia() {
